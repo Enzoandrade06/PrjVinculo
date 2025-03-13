81805cc4f073bc9b7b1f273827a8dfccf39c332b
aluno <aluno@aluno.com>
2025-03-12 21:47:52 -0300

    Adicionar arquivos de projeto.

---
+++ PrjVinculo/PrjVinculo/MainPage.xaml.cs
@@ -0,0 +1,51 @@
+﻿namespace PrjVinculo
+{
+    public partial class MainPage : ContentPage
+    {
+        int count = 0;
+
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void OnCounterClicked(object sender, EventArgs e)
+        {
+            count++;
+
+            if (count == 1)
+                CounterBtn.Text = $"Clicked {count} time";
+            else
+                CounterBtn.Text = $"Clicked {count} times";
+
+            SemanticScreenReader.Announce(CounterBtn.Text);
+        }
+
+        private void bntProduto_Clicked(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnProduto_Clicked(object sender, EventArgs e)
+        {
+            Produto produto = new Produto();
+
+            produto.Descricao = "Camiseta Real Madrid";
+            produto.Preco = 900;
+            produto.Categoria = "Vestimenta";
+            Navigation.PushAsync(new ProdutoPage() {BindingContext = produto});
+        }
+
+        private void btnProduto2_Clicked(object sender, EventArgs e)
+        {
+            Produto produto = new Produto();
+
+            produto.Descricao = "Camiseta PSG";
+            produto.Preco = 500;
+            produto.Categoria = "Vestimenta";
+            Navigation.PushAsync(new ProdutoPage2() {BindingContext = produto});
+        }
+    }
+
+}
---
+++ PrjVinculo/PrjVinculo/Produto.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrjVinculo
+{
+    internal class Produto
+    {
+        public string Descricao { get; set; }
+        public double Preco {  get; set; }
+        public string Categoria { get; set; }
+            
+    }
+}
